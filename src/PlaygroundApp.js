4ded376357e9a33c15f79ad5a721e9efb87ea046
Septian Widaryanto <septianwidaryanto@gmail.com>
2026-06-25 10:21:12 +0700

    perf(app): lazy load motion runtime

---
+++ src/PlaygroundApp.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { MotionConfig, useReducedMotion } from "framer-motion";
+import { EmilModalDemo } from "./components/EmilModalDemo.js";
+import { MotionExamples } from "./components/MotionExamples.js";
+import { ToastPanel } from "./components/ToastPanel.js";
+import { TopCornerProgress } from "./components/TopCornerProgress.js";
+import { UnifiedLoadingPanel } from "./components/UnifiedLoadingPanel.js";
+
+export function PlaygroundApp() {
+  const motionMode = "normal";
+  const prefersReducedMotion = useReducedMotion();
+  const [entranceReady, setEntranceReady] = useState(() => Boolean(prefersReducedMotion));
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      setEntranceReady(true);
+      return;
+    }
+    const id = requestAnimationFrame(() => setEntranceReady(true));
+    return () => cancelAnimationFrame(id);
+  }, [prefersReducedMotion]);
+
+  return React.createElement(
+    MotionConfig,
+    { reducedMotion: "user" },
+    React.createElement(
+      React.Fragment,
+      null,
+      React.createElement(TopCornerProgress, { motionMode, entranceReady }),
+      React.createElement(UnifiedLoadingPanel, { motionMode, entranceReady }),
+      React.createElement(ToastPanel, { motionMode, entranceReady }),
+      React.createElement(MotionExamples, { motionMode, entranceReady }),
+      React.createElement(EmilModalDemo)
+    )
+  );
+}
